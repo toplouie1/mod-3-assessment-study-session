bb33bbbf277794080c7eec8492314e24b8699bd5
Samantha Shapland <samshapland@gmail.com>
2021-12-15 13:42:34 -0500

    made App into class component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">hello world</div>;
+class App extends React.Component {
+  render() {
+    return <div className="App">hello world</div>;
+  }
 }
 
 export default App;
